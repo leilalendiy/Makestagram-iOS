952094ce65c48dfc959fc13203e1ac938b94e8b2
Leila Adaza <32406348+leilalendiy@users.noreply.github.com>
2018-07-10 09:53:49 -0700

    Sets up the basic app structure

---
+++ Makestagram/Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Leila Adaza on 7/10/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
