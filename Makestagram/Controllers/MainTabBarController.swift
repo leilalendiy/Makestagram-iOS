ea3b699ece90fa3a07e0d9dc9f08dd3bfa0f381c
Leila <leilalendiy@users.noreply.github.com>
2018-07-10 14:58:34 -0700

    Implements photo taking

---
+++ Makestagram/Controllers/MainTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarController.swift
+//  Makestagram
+//
+//  Created by Leila Adaza on 7/10/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    let photoHelper = MGPhotoHelper()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photoHelper.completionHandler = { image in
+            print("handle image")
+        }
+        
+        delegate = self
+        tabBar.unselectedItemTintColor = .black
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 1 {
+            photoHelper.presentActionSheet(from: self)
+            return false
+        }
+        
+        return true
+    }
+}
